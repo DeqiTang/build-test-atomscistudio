f98ec257fb6619c69206ddf689de0e938aeafcca
Deqi Tang <deqitang@gmail.com>
2022-03-03 11:30:57 +0800

    build

--- src/calc/calccontrol.cpp
+++ src/calc/calccontrol.cpp
@@ -54,8 +54,8 @@ CalcControl::CalcControl(QWidget *parent) : QWidget{parent} {
     atomsciflow::Ssh ssh;
 
     YAML::Node yaml_node = YAML::Load("[1, 2, 3, 4, 5]");
-    //for (int i = 0; i < yaml_node.size(); i++) {
-    //    std::cout << yaml_node[i].as<int>() << std::endl;
-    //}
+    for (int i = 0; i < yaml_node.size(); i++) {
+        std::cout << yaml_node[i].as<int>() << std::endl;
+    }
 
 }
