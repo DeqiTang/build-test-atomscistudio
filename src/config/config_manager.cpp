c174332532aa8c55c62a641911a1de244d0467e3
Deqi Tang <deqitang@gmail.com>
2022-03-05 21:21:58 +0800

    build

--- src/config/config_manager.cpp
+++ src/config/config_manager.cpp
@@ -1,5 +1,19 @@
 #include "config_manager.h"
 
+#include <cstdlib>
+
 ConfigManager::ConfigManager() {
 
+}
+
+
+std::string ConfigManager::get_home_dir() {
+    std::string home_dir;
+#ifdef _WIN32
+    char* home_path = std::get_env("HOMEPATH");
+    home_dir = std::string(home_paht);
+#elif defined(__linux__)
+    char* home = std::getenv("HOME");
+    home_dir = std::string(home);
+#endif
 }
--- src/config/config_manager.h
+++ src/config/config_manager.h
@@ -1,11 +1,13 @@
 #ifndef CONFIGMANAGER_H
 #define CONFIGMANAGER_H
 
+#include <string>
 
 class ConfigManager {
 public:
     ConfigManager();
 
+    std::string get_home_dir();
 };
 
 #endif // CONFIGMANAGER_H
