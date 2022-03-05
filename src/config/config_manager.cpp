471de8ab8253620338330bc9dc82815e87a35136
Deqi Tang <deqitang@gmail.com>
2022-03-05 21:25:15 +0800

    build

--- src/config/config_manager.cpp
+++ src/config/config_manager.cpp
@@ -12,7 +12,7 @@ std::string ConfigManager::get_home_dir() {
 #ifdef _WIN32
     char* home_path = std::get_env("HOMEPATH");
     home_dir = std::string(home_paht);
-#elif defined(__linux__)
+#elif defined(__linux__) || defined(__APPLE__)
     char* home = std::getenv("HOME");
     home_dir = std::string(home);
 #endif
